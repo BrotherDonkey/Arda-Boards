ceee052fbde7353c20fcd682937232a086451af1
Will Bjorn <willbjorn@gmail.com>
2017-05-17 21:18:09 +0000

    Deleted some unneccessary files

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Currently occurs only on /barad-dur route
+
 
 const endpoint = 'https://fdy-brotherdonkey.c9users.io/api-topics/';
 const searchInput = document.querySelector('#Barad-dur');
@@ -29,26 +31,24 @@ function displayMatches() {
     const matchArray = findMatches(this.value, searchTopics);
 
     const html = matchArray.map(topic => {
-       const regex = new RegExp(this.value, "gi");
-      const topicTitle = topic.title.replace(regex, `<span class="bg-success">${this.value}</span>`)
-       const topicText = topic.text.replace(regex, `<span class="bg-success">${this.value}</span>`)
+    const regex = new RegExp(this.value, "gi");
+    const topicTitle = topic.title.replace(regex, `<span class="bg-success">${this.value}</span>`)
+    const topicText = topic.text.replace(regex, `<span class="bg-success">${this.value}</span>`)
 
-       return `
-       <li class="card">
+    return `
+        <li class="card">
           <div class="card-block m-y-1">
             <a href="https://fdy-brotherdonkey.c9users.io/rivendell/${topic._id}">
-                <h4 class="card-title text-primary">${topicTitle}</h4>
+                <h4 class="card-title text-primary small">${topicTitle}</h4>
             </a>
-            <p class="card-text text-muted">${topicText}</span>
+            <p class="card-text text-muted small">${topicText}</span>
             </p>
           </div>
         </li>
-       `;
+        `;
     }).join('');
     suggestions.innerHTML = html;
 }
-
-
 
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
